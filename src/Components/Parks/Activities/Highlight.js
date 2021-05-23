86c51969c0569956afee956b71543e7c70c4fcef
Andrew Hansen <andrew89hansen@gmail.com>
2021-05-23 19:36:46 -0400

    add photo by

--- src/Components/Parks/Activities/Highlight.js
+++ src/Components/Parks/Activities/Highlight.js
@@ -1,7 +1,8 @@
 import React from 'react';
 
 const Highlight = ({ image, title }) => {    
-    const { profile_image, name, instagram_username, twitter_username } = image.user
+    const { profile_image, name, instagram_username, twitter_username } = image.user;
+    console.log("image", image);
     return (
         <>
             <div className={"activity__details"}>
@@ -10,19 +11,23 @@ const Highlight = ({ image, title }) => {
                 </h3>
                 <div className={"activity__details__user activity__user image-details"}>                                    
                     <div className={"image-details__user"}>
+                        <small className={"image-details__photoBy"}>Photo by:</small>
                         <img className={"image-details__user__headshot"} src={profile_image.medium} alt={name} />
                         <div className={"image-details__user__name"}>
                             {name}
                         </div>
-                        {twitter_username && <div className={"image-details__user__social"}>
-                            <a href={`https://twitter.com/${twitter_username}`}>
+                        {twitter_username && 
+                            <a className={"image-details__user__social"} href={`https://twitter.com/${twitter_username}`}>
                                 {`${twitter_username}`}
                             </a>
-                        </div>}
-                        {instagram_username && <div className={"image-details__user__social"}>
-                            <a href={`https://www.instagram.com/${instagram_username}`} target={"_blank"} rel="noopener noreferrer">
+                        }
+                        {instagram_username &&
+                            <a className={"image-details__user__social"} href={`https://www.instagram.com/${instagram_username}`} target={"_blank"} rel="noopener noreferrer">
                                 {`@${instagram_username}`}</a>
-                        </div>}
+                        }
+                        <a className={"image-details__user__social"} href={image.links.html}>
+                            View Full Size Image
+                        </a>
                     </div>
                 </div>
                 <img className={"activity__image"} src={image.urls.regular} alt={image.alt_description} />
