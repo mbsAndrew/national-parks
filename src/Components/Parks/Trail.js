abfdfa76b0153029781e0d27a4354bd1d452a026
Andrew Hansen <andrew89hansen@gmail.com>
2021-05-24 23:12:23 -0400

    side scroll on trails

--- src/Components/Parks/Trail.js
+++ src/Components/Parks/Trail.js
@@ -1,4 +1,4 @@
-import React, { useState } from 'react';
+import React, { useEffect, useState } from 'react';
 import { ChevronDown, ChevronUp, Map, Rulers, Triangle } from 'react-bootstrap-icons';
 
 const Title = ({children}) => {
@@ -33,7 +33,7 @@ const Info = ({ data, children }) => {
     const toggleOpen = () => {
         setOpenStatus(!isOpen);
     }
-    console.log(data);
+    
     return <div className={`trail__info ${isOpen && "trail__info--open"}`}>        
         <div className={"trail__toggle"} onClick={toggleOpen}>
             {children}
@@ -67,6 +67,44 @@ const Detail = ({ dataPoint, data, icon}) => {
     );
 }
 
+const Scroll = ({ direction = "left", onScroll}) => {   
+    const isLeft = direction === "left";
+    useEffect(() => {
+        return detach;
+    },[]);
+
+    function scrollAction(e) {
+        e.preventDefault();
+        e.stopPropagation();
+        const isUp = e.deltaY < 0;
+        let returnOffset = 0;        
+        if (isLeft && isUp) {
+            returnOffset = 20;
+        } else if (!isLeft && !isUp) {
+            returnOffset = -20;
+        }
+        onScroll(returnOffset);           
+    }
+
+    //attach the scroll event on entering and detach it after mouse leave
+    const attachScroll = () => {
+        console.log("attaching");
+        window.addEventListener("wheel", scrollAction, {passive: false});
+    }
+    const detach = () => {
+        console.log("detaching");
+        window.removeEventListener("wheel", scrollAction, {passive: false});
+    }
+
+    return (
+        <div onMouseEnter={attachScroll} onMouseLeave={detach} className={`trail__scroll trail__scroll_${direction}`}>
+            <span>
+                {isLeft ? "<" : ">"}
+            </span>
+        </div>
+    );
+};
+
 Trail.Title = Title;
 Trail.Subtitle = Subtitle;
 Trail.Image = Image;
@@ -76,9 +114,18 @@ Trail.Detail = Detail;
 
 export const TrailContainer = ({ data }) => {    
     //figure out scrolling here
+    const [offset, setOffset] = useState(0);
 
-    return <section className={"section section_fill-white"}>
-        <div className={"container_trail"}>
+    const onScroll = (val) => {
+        setOffset(prevState => {
+            return prevState + val;
+        });
+    }
+
+    return <section className={"section trail__section section_fill-white"}>
+        <Scroll onScroll={onScroll} />
+        <Scroll direction={"right"} onScroll={onScroll} />
+        <div style={{left: `${offset}px`, right: `${Math.abs(offset)}px`}} className={"trail__container"}>
             {data.map(item =>
                 <Trail>
                     <Trail.Image data={item}>
@@ -94,6 +141,7 @@ export const TrailContainer = ({ data }) => {
                 </Trail>
             )}
         </div>
+       
     </section>
     //use this to render instead of having everything
 };
