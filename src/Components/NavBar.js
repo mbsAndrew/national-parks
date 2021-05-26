8f8533488583a5db68ffa0dfb2fa5803da626723
Andrew Hansen <andrew89hansen@gmail.com>
2021-05-26 10:55:21 -0400

    waiting on unsplash approval.

--- src/Components/NavBar.js
+++ src/Components/NavBar.js
@@ -4,12 +4,10 @@ import { Link } from 'react-router-dom';
 
 export default function NavBar () {
     return (
-        <div className={"container"}>        
-            <div className={"row"}>
-                <div className={"col-12"}>
-                    <Link to={"/"} >Home</Link>                
-                </div>                
-            </div>
-        </div>
+        <div className={"nav"}>
+            <Link className={"nav__link"}>
+                Home
+            </Link>
+        </div>       
     );
 }
--- src/Components/Parks/Activities/Highlight.js
+++ src/Components/Parks/Activities/Highlight.js
@@ -3,19 +3,29 @@ import React from 'react';
 const Highlight = ({ image, title }) => {    
     const { profile_image, name, instagram_username, twitter_username } = image.user;
     console.log("image", image);
+
+    const referral = "?utm_source=national-parks&utm_medium=referral";
+    const userLink = `${image.user.links.html}${referral}`;
+    const unsplashLink = `https://unsplash.com/${referral}`;
+
     return (
         <>
             <div className={"activity__details"}>
                 <h3 className={"activity__details__title"}>
                     {title.name}
-                </h3>
+                </h3>                
                 <div className={"activity__details__user activity__user image-details"}>                                    
                     <div className={"image-details__user"}>
                         <small className={"image-details__photoBy"}>Photo by:</small>
                         <img className={"image-details__user__headshot"} src={profile_image.medium} alt={name} />
                         <div className={"image-details__user__name"}>
-                            {name}
+                            <a className={"image-details__user__link"} href={userLink} target={"_blank"}>
+                                {name}
+                            </a>
                         </div>
+                        <a className={"image-details__user__link"} href={unsplashLink} target={"_blank"}>
+                            on Unsplash
+                        </a>
                         {twitter_username && 
                             <a className={"image-details__user__social"} href={`https://twitter.com/${twitter_username}`}>
                                 {`${twitter_username}`}
