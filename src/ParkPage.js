7c0c2158e125f9e024ab7817a130c7574764c510
Andrew Hansen <andrew89hansen@gmail.com>
2021-06-04 17:17:14 -0400

    Convert to func, add Section comp.

--- src/ParkPage.js
+++ src/ParkPage.js
@@ -1,4 +1,4 @@
-import React from 'react';
+import React, { useEffect, useRef, useState } from 'react';
 import Activities from './Components/Parks/Activities/Activities';
 import Location from './Components/Parks/Location';
 import Hero from './Components/Hero/Hero';
@@ -7,82 +7,97 @@ import Hiking from './Components/Parks/Hiking';
 import BackgroundImage from './Components/BackgroundImage';
 import { API_URL } from './services/url';
 import Articles from './Components/Parks/Articles/Articles';
+import Section from './Components/Section';
 
-class ParkPage extends React.Component {
-    constructor() {
-        super();
-
-        this.state = {
-            data: {}
-        }
+const ParkPage = ({ location, match }) => {
+    const [data, setData] = useState({});
+    const newsRef = useRef();
+    const activitiesRef = useRef();
+    const hikingRef = useRef();
+    const locationRef = useRef();
+    
+    const refs = {
+        NEWS: { text: "News", ref: newsRef},
+        ACTIVITES: {text: "Activities", ref: activitiesRef},
+        HIKING: {text: "Hiking", ref: hikingRef},
+        LOCATION: {text: "Location & Hours", ref: locationRef}
     }
 
-    componentDidMount() {        
-        const { data } = this.props.location.state || false;
-        if (data) {            
-            this.setState({
-                data: data
-            })
+    useEffect(() => {
+        console.log(location);
+        if (location.state?.data) {
+            setData(data);
         } else {
-            this.initData();
-        }        
-    }
+            initData();
+        }
+    }, []);
 
-    initData = () => {
-        const { parkID } = this.props.match.params;                
+    //get initial data about the park
+    function initData() {
+        const { parkID } = match.params;
         fetch(`${API_URL}/info/${parkID}`)
             .then(res => res.json())
-            .then(data => {
-                console.log("infoData", data);
-                this.setState({
-                    data: data.data[0]
-                });
+            .then(data => {           
+                console.log(data);     
+                setData(data.data[0]);                
             })
     }
 
-    randomNum = (min, max) => {
+    //select background image for hero from images provided by NPS
+    const selectHeroBackground = (imgs) => {        
+        return imgs[randomNum(0, imgs.length - 1)].url;
+    }
+
+    const randomNum = (min, max) => {
         return Math.round(Math.random() * (max - min) + min);
     }
 
-    selectHeroBackground = (imgs) => {        
-        return imgs[this.randomNum(0, imgs.length - 1)].url;
+    //scrolls to the div for the information
+    const scrollToDiv = (ref) => {        
+        ref.current && ref.current.scrollIntoView({
+            behavior: "smooth"
+        });
     }
-
-    render() {
-        const { data } = this.state;        
-        const { parkID } = this.props.match.params;          
-        return (
-            <>
-                {Object.keys(data).length > 0 ?
-                    <>                        
-                            <Hero
-                                isFull
-                            >
-                            <BackgroundImage src={this.selectHeroBackground(data.images)}>   
-                                <Hero.Container>
-                                    <Hero.Title>
-                                        {data.name}
-                                    </Hero.Title>
-                                    <Hero.Subtitle>
-                                        {data.designation}
-                                    </Hero.Subtitle>
-                                    <Hero.Description>
-                                        {data.description}
-                                    </Hero.Description>                                   
-                                    <HeroOptions data={data} />
-                                </Hero.Container>                      
-                            </BackgroundImage>                                
-                            </Hero>                                                
-                        {data.addresses && <Location addresses={data.addresses} location={{lat: data.latitude, long: data.longitude}} />}
-                        {/* <Hiking lat={data.latitude} long={data.longitude} /> */}
-                        {data.activities && <Activities list={data.activities} />}
-                        <Articles parkID={parkID} />
-                        {data.topics && <Activities list={data.topics} title={"Topics"} />}                                                                      
-                    </>
-                    : "No data son!"}
-            </>
-        )
-    }
+    
+    return (
+        <>
+            {Object.keys(data).length > 0 ?
+                <>                        
+                        <Hero
+                            isFull
+                        >
+                        <BackgroundImage src={selectHeroBackground(data.images)}>   
+                            <Hero.Container>
+                                <Hero.Title>
+                                    {data.name}
+                                </Hero.Title>
+                                <Hero.Subtitle>
+                                    {data.designation}
+                                </Hero.Subtitle>
+                                <Hero.Description>
+                                    {data.description}
+                                </Hero.Description>                                   
+                                <HeroOptions onClick={scrollToDiv} refData={refs} data={data} />
+                            </Hero.Container>                      
+                        </BackgroundImage>                                
+                        </Hero>
+                        <Section ref={locationRef} name={"location"}>
+                            {data.addresses && <Location addresses={data.addresses} location={{lat: data.latitude, long: data.longitude}} />}                        
+                        </Section>               
+                        {/* <Section ref={hikingRef} name={"hiking"}>
+                            <Hiking lat={data.latitude} long={data.longitude} />
+                        </Section> */}
+                        <Section ref={activitiesRef} name={"activites"}>
+                            {data.activities.length > 0 && <Activities list={data.activities} />}
+                        </Section>
+                        <Section ref={newsRef} name={"news"}>
+                            <Articles parkID={match.params.parkID} />
+                        </Section>
+                        {data.topics.length > 0 && <Activities list={data.topics} title={"Topics"} />}                                                                      
+                </>
+                : "No data son!"}
+        </>
+    )
 }
 
 export default ParkPage;
